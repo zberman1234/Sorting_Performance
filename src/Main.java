d1130ed77d64b3d99ae74f0dd29ab89130e88e86
Zach Berman <zberman2023@kehillahstudent.org>
2022-12-19 11:03:49 -0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,193 @@
+import java.util.Arrays;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartFrame;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        int maxN = 200000;
+        int interval = 500;
+        Point[] points = new Point[maxN/interval];
+        int idx = 0;
+        for(int n = 1; n < maxN; n+=interval) {
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = (int) (Math.random() * 100);
+            }
+            long start = System.nanoTime();
+            // quicksort(arr, 0, arr.length - 1);
+            // heapSort(arr);
+            radixsort(arr, arr.length);
+
+            long end = System.nanoTime();
+            long time = end - start;
+            points[idx] = new Point(n, time);
+            idx++;
+        }
+
+    
+
+        XYSeries series = new XYSeries("Data");
+        for (Point p : points) {
+           series.add(p.getX(), p.getY());
+        }
+        XYSeriesCollection dataset = new XYSeriesCollection(series);
+        JFreeChart chart = ChartFactory.createScatterPlot("radixsort vs n", "n", "Time", dataset, PlotOrientation.VERTICAL, false, true, true);
+        // JFreeChart chart = ChartFactory.createScatterPlot("heapsort vs n", "n", "Time", dataset, PlotOrientation.VERTICAL, false, true, true);
+        // JFreeChart chart = ChartFactory.createScatterPlot("Quicksort vs n", "n", "Time", dataset, PlotOrientation.VERTICAL, false, true, true);
+        // graph the line of best fit using getOLSRegression()
+        
+
+        //draw the chart
+        ChartFrame frame = new ChartFrame("Chart", chart);
+        frame.pack();
+        frame.setVisible(true);
+
+        
+
+        // int[] arr1 = {3, 7, 1, 3, 2, 1, 8, 12, 2};
+        // heapSort(arr1);
+        // p(Arrays.toString(arr1));
+
+        // int[] arr2 = {3, 7, 1, 3, 2, 1, 8, 12, 2};
+        // radixsort(arr2, arr2.length);
+        // p(Arrays.toString(arr2));
+    }
+
+    public static void quicksort(int[] arr, int left, int right) {
+        if (left < right) {
+            int pivot = partition(arr, left, right);
+            quicksort(arr, left, pivot - 1);
+            quicksort(arr, pivot + 1, right);
+        }
+    }
+
+    public static int partition(int[] arr, int left, int right) {
+        int pivot = arr[right];
+        int i = left - 1;
+        for (int j = left; j < right; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[right];
+        arr[right] = temp;
+        return i + 1;
+    }
+
+
+    public static void heapSort(int[] array) {
+        int n = array.length;
+
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(array, n, i);
+
+        // One by one extract an element from heap
+        for (int i = n - 1; i >= 0; i--) {
+            // Move current root to end
+            int temp = array[0];
+            array[0] = array[i];
+            array[i] = temp;
+
+            // call max heapify on the reduced heap
+            heapify(array, i, 0);
+        }
+    }
+
+    // To heapify a subtree rooted with node i which is
+    // an index in arr[]. n is size of heap
+    public static void heapify(int[] array, int n, int i) {
+        int largest = i; // Initialize largest as root
+        int l = 2 * i + 1; // left = 2*i + 1
+        int r = 2 * i + 2; // right = 2*i + 2
+
+        // If left child is larger than root
+        if (l < n && array[l] > array[largest])
+            largest = l;
+
+        // If right child is larger than largest so far
+        if (r < n && array[r] > array[largest])
+            largest = r;
+
+        // If largest is not root
+        if (largest != i) {
+            int swap = array[i];
+            array[i] = array[largest];
+            array[largest] = swap;
+
+            // Recursively heapify the affected sub-tree
+            heapify(array, n, largest);
+        }
+    }
+
+    
+    // A utility function to get maximum value in arr[]
+    static int getMax(int arr[], int n)
+    {
+        int mx = arr[0];
+        for (int i = 1; i < n; i++)
+            if (arr[i] > mx)
+                mx = arr[i];
+        return mx;
+    }
+ 
+    // A function to do counting sort of arr[] according to
+    // the digit represented by exp.
+    static void countSort(int arr[], int n, int exp)
+    {
+        int output[] = new int[n]; // output array
+        int i;
+        int count[] = new int[10];
+        Arrays.fill(count,0);
+ 
+        // Store count of occurrences in count[]
+        for (i = 0; i < n; i++)
+            count[ (arr[i]/exp)%10 ]++;
+ 
+        // Change count[i] so that count[i] now contains
+        // actual position of this digit in output[]
+        for (i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+ 
+        // Build the output array
+        for (i = n - 1; i >= 0; i--)
+        {
+            output[count[ (arr[i]/exp)%10 ] - 1] = arr[i];
+            count[ (arr[i]/exp)%10 ]--;
+        }
+ 
+        // Copy the output array to arr[], so that arr[] now
+        // contains sorted numbers according to current digit
+        for (i = 0; i < n; i++)
+            arr[i] = output[i];
+    }
+ 
+    // The main function to that sorts arr[] of size n using
+    // Radix Sort
+    static void radixsort(int arr[], int n)
+    {
+        // Find the maximum number to know number of digits
+        int m = getMax(arr, n);
+ 
+        // Do counting sort for every digit. Note that instead
+        // of passing digit number, exp is passed. exp is 10^i
+        // where i is current digit number
+        for (int exp = 1; m/exp > 0; exp *= 10)
+            countSort(arr, n, exp);
+    }
+
+    public static void p(Object o) {
+        System.out.println(o);
+    }
+
+
+}
---
+++ src/Point.java
@@ -0,0 +1,18 @@
+public class Point {
+    //coordinate class
+    private int x;
+    private long y;
+
+    public Point(int x, long y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public long getY() {
+        return y;
+    }
+}
